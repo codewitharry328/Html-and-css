3e2f3a5e2de9c6a60e62c5b980f27e9d575cc662
Amit meena <mea55629@gmail.com>
2024-09-12 13:09:23 +0530

    Add files via upload

---
+++ Hii.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+
+int main() {
+    // for(float i=1.0; i<=5.0; i++) {
+    // printf("%f\n", i);
+    // }
+
+    // for(char ch='a'; ch<='z'; ch++) {
+    // printf("%c\n", ch);
+    // }
+
+    // int age = 22;
+    // printf("age is %d\n", age);
+
+    // float pi = 3.99;
+    // printf("age is %f\n", pi);
+
+    // printf("%d \n", 4 != 4);
+    // printf("%d \n", 4 == 4);
+
+    // printf("%d \n", 3>4 && 5>2);
+    // printf("%d \n", 3>4 || 5>2);
+
+    // !not
+    // printf("%d \n", !( (5>1) && (3>4) ));
+
+    // assignment operator +, -, *, /
+
+    // int a = 1;
+    // int b = 4;
+    // a = a + b;
+    // printf("%d\n", a);
+
+    // int isSunday = 0;
+    // int isSnowing = 1;
+    // printf("%d\n", isSunday && isSnowing);
+
+    // int x = 14;
+    // int y = 18;
+
+    // if (14 > 18) {
+    // printf("20 is greater than 18");
+    // }
+
+    // else if(14 > 18 && 18 < 14) {
+    // printf("18 is greater than 20");
+    // }
+
+    // else {
+    // printf("18 is greater than 20");
+    // }
+
+    /*int day = 5;
+
+switch (day) {
+  case 1:
+    printf("Monday");
+     break;
+  case 2:
+    printf("Tuesday");
+     break;
+  case 3:
+    printf("Wednesday");
+     break;
+  case 4:
+    printf("Thursday");
+     break;
+  case 5:
+    printf("Friday");
+     break;
+  case 6:
+    printf("Saturday");
+     break;
+  case 7:
+    printf("Sunday");
+     break;
+}*/
+
+    return 0;
+}
