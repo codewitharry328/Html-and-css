c6c3735febb15c2c5004399ecb692225216e530c
Amit meena <mea55629@gmail.com>
2024-09-18 17:47:38 +0530

    Add files via upload

---
+++ Amit.js
@@ -0,0 +1,27 @@
+let input = document.getElementById('input');
+let button = document.querySelector('button');
+
+let string = "";
+let arr = Array.from('button');
+arr.forEach(button => {
+    button.addEventlistener('click', (e) => {
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
